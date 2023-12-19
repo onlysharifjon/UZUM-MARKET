02a4865721844e9fa800816c1a7fbc5a83cc4347
diyas08 <newkingg08@gmail.com>
2023-12-19 13:49:34 +0500

    saler app

--- SalerApp/models.py
+++ SalerApp/models.py
@@ -2,13 +2,3 @@ from django.db import models
 
 # Create your models here.
 
-class SalerRegister(models.Model):
-    login = models.CharField(max_lenght=18,unique=True)
-    password = models.CharField(max_lenght=16)
-    name = models.CharField(max_length=20)
-    surname = models.CharField(max_length=20)
-    phone = models.IntegerField(unique=True)
-    PasportSeria = models.CharField(max_length=2)
-    PasportNumber = models.IntegerField(unique=True)
-
-
