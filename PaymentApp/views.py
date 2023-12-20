c56dc79c2adbe979e56598859952d918b47174b0
begi202000 <bekzodhotamov123@gmail.com>
2023-12-20 16:14:57 +0500

    bekzod pushed

---
+++ PaymentApp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.shortcuts import render
+from rest_framework import status
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+# Create your views here.
+
+# class CardAdd(APIView):
