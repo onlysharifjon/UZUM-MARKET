82e07410b8e46af8d5fee44750f915c88f1248b3
onlysharifjon <mominovsharif12@gmail.com>
2024-01-19 11:47:53 +0500

    Last update

--- ProductAPP/views.py
+++ ProductAPP/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+
+from .models import KatalogModel
+
+def filtr_by_katalog(katalog_bot):
+    categorylar = KatalogModel.objects.all().filter(katalog=katalog_bot)
+    return categorylar
+
+
 # Create your views here.
--- bot/main.py
+++ bot/main.py
@@ -5,7 +5,14 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.types import CallbackQuery
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from keyboards.default import keyboard
-from keyboards.inline import Katalog1,Katalog2
+from keyboards.inline import Katalog1, Katalog2
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Core.settings')
+application = get_wsgi_application()
+# ------------------------DJANGO--------------------
+from ProductAPP.views import filtr_by_katalog
 
 API_TOKEN = '6029555538:AAGZVSM6OIOoomI92pIcy5Zm7tk4MtFR_Ys'
 logging.basicConfig(level=logging.INFO)
@@ -21,6 +28,37 @@ async def startt(message: types.Message):
 @dp.message_handler(text='Katalog')
 async def katalog(message: types.Message):
     await message.answer('KATALOG', reply_markup=Katalog1)
+
+
+@dp.callback_query_handler(text='oldinga')
+async def oldinga_inline(call: types.CallbackQuery):
+    try:
+        await bot.edit_message_reply_markup(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                            reply_markup=Katalog2)
+    except:
+        await call.answer('Boshqa menyu mavjud emas')
+
+
+@dp.callback_query_handler(text='orqaga')
+async def orqaga_inline(call: types.CallbackQuery):
+    try:
+        await bot.edit_message_reply_markup(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                            reply_markup=Katalog1)
+    except:
+        await call.answer('Boshqa menyu mavjud emas')
+
+
+from channels.db import database_sync_to_async
+
+
+@dp.callback_query_handler()
+async def catalog_oladi(call: types.CallbackQuery):
+    print(call.data)
+    # categorylar = filtr_by_katalog(call.data)
+    # send callback data to django
+    categorylar = await database_sync_to_async(filtr_by_katalog)(call.data)
+    #get data from django
+
 
 
 if __name__ == '__main__':
